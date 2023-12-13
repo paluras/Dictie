b354b4eabe1dc1c16342f62b8f877bc4d45d48ea
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-12-13 19:05:15 +0200

    Fix refreshing state behavior , pls work on phone

--- dictie/src/utils/handleVoice.tsx
+++ dictie/src/utils/handleVoice.tsx
@@ -1,5 +1,6 @@
 import levenshteinDistance from "./levensheinDistance";
 import filterAlphabetic from "./filterAlphabetic";
+import { set } from "astro/zod";
 // This needs to be refactored'
 // Have the speechRecognition be one thing
 // DOM Manipulation another thing
@@ -46,43 +47,34 @@ const handleVoiceInput = ({
 
   recognition.onaudiostart = () => {
     setSpeechEnd(false);
-    // Make the background a color
-    // refactor this
     documentBody.style.backgroundColor = "#E5E581";
     startButton.style.backgroundColor = "#79d2c4";
-    console.log("Audio started");
-  };
-  recognition.onspeechend = () => {
-    recognition.stop();
-   
-      setSpeechEnd(true);
-  
-   
-    console.log("Speech recognition has stopped.");
+    console.log(score,"Audio started");
   };
 
-  // Change Background when audio ends
+
+  recognition.onspeechend = () => {
+    recognition.stop();
+      setSpeechEnd(true);
+    console.log(score,"On Speech End Ran");
+  };
+
+
   recognition.onaudioend = () => {
     recognition.stop();
-    console.log("On Audio End Ran");
     documentBody.style.backgroundColor = "#fafaf0";
     startButton.style.backgroundColor = "#E5E580";
     setSpeechEnd(true);
-
-    // console.log(userScore, "userScore");
-
-
+    console.log(score,"On Audio End Ran");
 
     
-
+    setTimeout(() => {
     if (speechEnd && score >= 10 && score < 90) {
-  
-      setTimeout(() => {
         handleFront();
-        console.log("speechEnd && score >= 10 && score < 90 Ran");
-      }, 1000);
+        console.log(score,"speechEnd && score >= 10 && score < 90 Ran");
     }
     recognition.abort();
+  }, 1000);
   };
   // Stop button
   const stopBtn: HTMLElement = document.querySelector(".stop") as HTMLElement;
@@ -95,7 +87,7 @@ const handleVoiceInput = ({
   recognition.onresult = (event: { results: { transcript: string }[][] }) => {
     const transcript: string = event.results[0][0].transcript;
     setSpokenText(transcript); // Added setSpokenText to update spokenText prop
-    console.log(transcript);
+ 
 
     // Calculate Levenshtein distance and similarity percentage
     const distance = levenshteinDistance(
@@ -106,26 +98,32 @@ const handleVoiceInput = ({
     const maxLength = Math.max(transcript.length, givenText.length);
     const similarity = Math.max(0, (1 - distance / maxLength) * 100);
     score = Math.round(similarity);
-    console.log(score, "score");
+   
 
     setSimilarityPercentage(similarity); // Added setSimilarityPercentage to update similarityPercentage prop
     
     if (speechEnd && score >= 90) {
-      setTimeout(() => {
-        console.log("speechEnd && score >= 90 Ran");
+     
+        console.log(score,"speechEnd && score >= 90 Ran");
         
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         //@ts-expect-error
         setUserScore((prev: number) => prev + 1);
-        handleFront();
-      }, 1000);
+        console.log(score,"Score updated")
+    setTimeout(()=>{
+      handleFront();
+    },1000)    
+      
+     
       recognition.stop();
 
     }else if(recognition.onspeechend && score < 10){
-      console.log("Speech ended");
-      
+      console.log(score,"Speech ended");
       recognition.stop();
     }
+
+   
+ 
  
 
     
